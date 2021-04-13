0ecf6954163d3da69d8efafc2983b380bbfa6668
Julio Corzo <juliocorzo@me.com>
2021-04-12 18:47:09 -0700

    Simple user generation for login endpoint validation.

--- server/src/main/java/com/cooksys/server/DatabaseSeeder.java
+++ server/src/main/java/com/cooksys/server/DatabaseSeeder.java
@@ -1,1 +1,36 @@
 package com.cooksys.server;
+
+import com.cooksys.server.entities.Credential;
+import com.cooksys.server.entities.Profile;
+import com.cooksys.server.entities.User;
+import com.cooksys.server.repositories.UserRepository;
+import lombok.AllArgsConstructor;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.stereotype.Component;
+
+@Component
+@AllArgsConstructor
+public class DatabaseSeeder implements CommandLineRunner {
+
+    private final UserRepository userRepository;
+
+    @Override
+    public void run(String... args) {
+
+        Profile profile = new Profile();
+        profile.setFirstName("Michael");
+        profile.setLastName("Degg");
+        profile.setPhone("478-832-2687");
+
+        Credential credential = new Credential();
+        credential.setEmail("test@test.com");
+        credential.setPassword("password");
+
+        User user = new User();
+        user.setCredentials(credential);
+        user.setProfile(profile);
+
+        userRepository.saveAndFlush(user);
+
+    }
+}
