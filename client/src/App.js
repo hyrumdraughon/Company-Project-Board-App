03a9340537803681942acdb1d5f7b623c4680bc0
therandomfool <mddenver@protonmail.com>
2021-04-13 08:45:05 -0700

    Finished Button component

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,17 @@
+import React from 'react'
+
 import Button from './components/Button'
+
 import './App.css';
+import './index.css'
+import './styles/Button.css';
+
 
 function App() {
   return (
+    <div>
     <Button></Button>
+    </div>
   );
 }
 
