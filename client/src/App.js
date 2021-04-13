f5b0180db7308335b901ff6d0395852d3527dcf6
therandomfool <mddenver@protonmail.com>
2021-04-12 22:36:37 -0700

    Started ProjectCard

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,14 @@
 import Button from './components/Button'
+import 'bootstrap/dist/css/bootstrap.min.css';
+import ProjectCard from './components/ProjectCard'
 import './App.css';
 
 function App() {
   return (
-    <Button></Button>
+    <>
+      <Button></Button>
+      <ProjectCard></ProjectCard>
+    </>
   );
 }
 
