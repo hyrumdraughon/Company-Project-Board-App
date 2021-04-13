a004fa09649b9ee83c24ee5a49eb02e7e77bd935
therandomfool <mddenver@protonmail.com>
2021-04-13 08:49:57 -0700

    Resolved merge conflicts

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,7 @@
 import React from 'react'
 
 import Button from './components/Button'
-<<<<<<< HEAD
-import 'bootstrap/dist/css/bootstrap.min.css';
-import ProjectCard from './components/ProjectCard'
-=======
 
->>>>>>> iss11
 import './App.css';
 import './index.css'
 import './styles/Button.css';
@@ -14,16 +9,11 @@ import './styles/Button.css';
 
 function App() {
   return (
-<<<<<<< HEAD
-    <>
-      <Button></Button>
-      <ProjectCard></ProjectCard>
-    </>
-=======
+
     <div>
     <Button></Button>
     </div>
->>>>>>> iss11
+
   );
 }
 
