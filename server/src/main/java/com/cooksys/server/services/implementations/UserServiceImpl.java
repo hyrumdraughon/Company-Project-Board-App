f406e690275656aa7a7538f335488d1d657fffb1
Julio Corzo <juliocorzo@me.com>
2021-04-13 15:21:44 -0700

    Fixed some issues with createUser. (#8)

--- server/src/main/java/com/cooksys/server/services/implementations/UserServiceImpl.java
+++ server/src/main/java/com/cooksys/server/services/implementations/UserServiceImpl.java
@@ -4,6 +4,10 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 
+import com.cooksys.server.entities.Company;
+import com.cooksys.server.entities.Credential;
+import com.cooksys.server.entities.Profile;
+import com.cooksys.server.exceptions.UnauthorizedException;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.HttpStatusCodeException;
 
@@ -33,15 +37,15 @@ import lombok.AllArgsConstructor;
 @AllArgsConstructor
 public class UserServiceImpl implements UserService {
 
-	private UserRepository userRepo;
-	private UserMapper userMapper;
-	private CredentialMapper credential;
-	private ProfileMapper profile;
-	private CompanyRepository companies;
-	private TeamRepository teamRepo;
-	private RoleRepository roleRepo;
-	private RoleMapper rm;
-	private TeamMapper tm;
+	private final UserRepository userRepository;
+	private final UserMapper userMapper;
+	private final CredentialMapper credentialMapper;
+	private final ProfileMapper profileMapper;
+	private final CompanyRepository companyRepository;
+	private final TeamRepository teamRepository;
+	private final RoleRepository roleRepository;
+	private final RoleMapper roleMapper;
+	private final TeamMapper teamMapper;
 
 	// Takes an Id and and a User Object. if Optional User is empty the user doesnt
 	// exist
@@ -60,7 +64,7 @@ public class UserServiceImpl implements UserService {
 		if (id == null) {
 			throw new BadRequestException("ID cannot be empty or null was provided: " + id);
 		}
-		Optional<User> optionalUser = userRepo.findById(id);
+		Optional<User> optionalUser = userRepository.findById(id);
 		// User Validation to check to see if the user exists.
 		checkExistsNotDeleted(optionalUser.get().getId(), optionalUser);
 
@@ -72,7 +76,7 @@ public class UserServiceImpl implements UserService {
 	public List<UserDto> getAllUsers() {
 		// Returns every active user in the repository. Mostly for testing Purposes.
 		List<UserDto> activeUsers = new ArrayList<>();
-		for (User u : userRepo.findAll()) {
+		for (User u : userRepository.findAll()) {
 			if (u.isActive()) {
 				activeUsers.add(userMapper.entityToDto(u));
 			}
@@ -80,31 +84,28 @@ public class UserServiceImpl implements UserService {
 		return activeUsers;
 	}
 
+	// Endpoint tested.
 	@Override
 	public UserDto createUser(CreateUserDto createUserDto) {
-		// Creates a new user. If any required fields are missing or the email
-		// provided is already taken, an error should be sent in lieu of a response.
-		// If the given credentials match a previously-deleted user, re-activate the
-		// deleted user instead of creating a new one.
-		if (userRepo.equals(createUserDto.getCredentials().getEmail())) {
-			User user = userRepo.findByCredentialsEmail(createUserDto.getCredentials().getEmail());
-			if (!user.isActive()) {
-				user.setActive(true);
-				return userMapper.entityToDto(user);
-			}
-		} else if (createUserDto.getCredentials() == null || createUserDto.getProfile() == null) {
-			throw new BadRequestException("You need to provide both credentials and Profile to create a user");
-		} else {
-			User user = new User();
-			// Save all relevant information.
-			user.setCredentials(credential.dtoToEmbeddable(createUserDto.getCredentials()));
-			user.setProfile(profile.dtoToEmbeddable(createUserDto.getProfile()));
-			user.setUserCompany(companies.getOne(createUserDto.getCompanyId()));
-			user.setActive(true);
 
-			return userMapper.entityToDto(userRepo.saveAndFlush(user));
-		}
-		throw new NullPointerException("Something went wrong, try again.");
+		checkCreateUserDtoForNull(createUserDto);
+
+		Credential credentials = credentialMapper.dtoToEmbeddable(createUserDto.getCredentials());
+		Profile createdProfile = profileMapper.dtoToEmbeddable(createUserDto.getProfile());
+
+		checkEmailAvailability(credentials.getEmail());
+
+		Company databaseCompany = getCompanyById(createUserDto.getCompanyId());
+
+		User createdUser = new User();
+		createdUser.setCredentials(credentials);
+		createdUser.setProfile(createdProfile);
+		createdUser.setUserCompany(databaseCompany);
+		createdUser.setActive(true);
+		createdUser = userRepository.saveAndFlush(createdUser);
+
+		return userMapper.entityToDto(createdUser);
+
 	}
 
 	@Override
@@ -113,12 +114,12 @@ public class UserServiceImpl implements UserService {
 			throw new NotFoundException("Must Provide a user ID");
 		}
 		// check user exists and is not deleted
-		Optional<User> optUser = userRepo.findById(id);
+		Optional<User> optUser = userRepository.findById(id);
 		checkExistsNotDeleted(id, optUser);
 
 		// update user's profile, flush, and return UserDto
-		optUser.get().setProfile(profile.dtoToEmbeddable(createUserDto.getProfile()));
-		userRepo.flush();
+		optUser.get().setProfile(profileMapper.dtoToEmbeddable(createUserDto.getProfile()));
+		userRepository.flush();
 		return userMapper.entityToDto(optUser.get());
 	}
 
@@ -128,7 +129,7 @@ public class UserServiceImpl implements UserService {
 			throw new NotFoundException("Must Provide a user ID");
 		}
 		// Check user exists and is not deleted
-		Optional<User> optUser = userRepo.findById(id);
+		Optional<User> optUser = userRepository.findById(id);
 		if (optUser.isEmpty()) {
 			throw new NotFoundException("The user with id: " + id + " does not exist");
 		} else if (!optUser.get().isActive()) {
@@ -136,7 +137,7 @@ public class UserServiceImpl implements UserService {
 		}
 
 		optUser.get().setActive(false);
-		userRepo.flush();
+		userRepository.flush();
 		return userMapper.entityToDto(optUser.get());
 	}
 
@@ -145,16 +146,16 @@ public class UserServiceImpl implements UserService {
 		if (id == null) {
 			throw new NotFoundException("Must Provide a user ID");
 		}
-		Optional<User> optUser = userRepo.findById(id);
+		Optional<User> optUser = userRepository.findById(id);
 		checkExistsNotDeleted(id, optUser);
 		if (role.getId() != 1 && role.getId() != 2) {
 			throw new BadRequestException("User can only be admin, or user");
 		} else {
-			Optional<Role> newRole = roleRepo.findById(role.getId()); //saveAndFlush(rm.DtoToEntity(role));
+			Optional<Role> newRole = roleRepository.findById(role.getId()); //saveAndFlush(roleMapper.DtoToEntity(role));
 			
 			optUser.get().setRole(newRole.get());
 			System.out.println(role.getName());
-			return userMapper.entityToDto(userRepo.saveAndFlush(optUser.get()));
+			return userMapper.entityToDto(userRepository.saveAndFlush(optUser.get()));
 		}
 
 	}
@@ -164,15 +165,76 @@ public class UserServiceImpl implements UserService {
 		if (id == null) {
 			throw new NotFoundException("Must Provide a user ID");
 		}
-		Optional<User> optUser = userRepo.findById(id);
+		Optional<User> optUser = userRepository.findById(id);
 		checkExistsNotDeleted(id, optUser);
-		if (teamRepo.findById(team.getId()).isEmpty()) {
+		if (teamRepository.findById(team.getId()).isEmpty()) {
 			throw new NotFoundException("Team Does not Exist");
 		} else {
-			optUser.get().setUserTeam(teamRepo.saveAndFlush(tm.DtoToEntity(team)));
-			return userMapper.entityToDto(userRepo.saveAndFlush(optUser.get()));
+			optUser.get().setUserTeam(teamRepository.saveAndFlush(teamMapper.DtoToEntity(team)));
+			return userMapper.entityToDto(userRepository.saveAndFlush(optUser.get()));
 		}
 
+	}
+
+	// Helper methods
+
+	// Checks for all possible null values when creating a user.
+	private void checkCreateUserDtoForNull(CreateUserDto createUserDto) {
+
+		if(createUserDto == null) {
+			throw new BadRequestException("Request body not provided.");
+		}
+
+		if(createUserDto.getProfile() == null) {
+			throw new BadRequestException("Profile information must be provided.");
+		}
+
+		if(createUserDto.getProfile().getFirstName() == null) {
+			throw new BadRequestException("First name must be provided.");
+		}
+
+		if(createUserDto.getProfile().getLastName() == null) {
+			throw new BadRequestException("Last name must be provided.");
+		}
+
+		if(createUserDto.getProfile().getPhone() == null) {
+			throw new BadRequestException("Phone number must be provided.");
+		}
+
+		if(createUserDto.getCredentials() == null) {
+			throw new BadRequestException("Credentials must be provided.");
+		}
+
+		if(createUserDto.getCredentials().getEmail() == null) {
+			throw new BadRequestException("Email must be provided.");
+		}
+
+		if(createUserDto.getCredentials().getPassword() == null) {
+			throw new BadRequestException("Password must be provided.");
+		}
+
+		if(createUserDto.getCompanyId() == null) {
+			throw new BadRequestException("Company ID must be provided.");
+		}
+	}
+
+	// Returns the company that corresponds to the provided ID, otherwise throws not found exception.
+	private Company getCompanyById(Long companyId) {
+		for(Company company : companyRepository.findAll()) {
+			if(company.getId().equals(companyId)) {
+				return company;
+			}
+		}
+		throw new NotFoundException("Company with that ID does not exist.");
+	}
+
+	// Checks if email is available in the database, otherwise throws unauthorized exception.
+	private void checkEmailAvailability(String email) {
+		for(User user : userRepository.findAll()) {
+			if(user.getCredentials().getEmail().equals(email)) {
+				throw new UnauthorizedException("User with this email already exists.");
+			}
+		}
 	}
 
 }
