63d0cd901bb5f56aa2644a679d915ddd0a17aae4
Justin Reidy <justintreidy@gmail.com>
2021-04-14 10:56:58 -0500

    Created UserProvider, still need to implement signup function

--- client/src/index.js
+++ client/src/index.js
@@ -2,12 +2,16 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import { BrowserRouter } from "react-router-dom"
+import UserProvider from './context/UserProvider'
 import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <BrowserRouter>
+    <UserProvider>
+      <App />
+    </UserProvider>
+  </BrowserRouter>,
   document.getElementById('root')
 );
 
