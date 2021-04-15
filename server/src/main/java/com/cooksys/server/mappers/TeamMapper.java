03e93ab24cdeffc982e772fcbf5d8d1344dff723
Julio Corzo <juliocorzo@me.com>
2021-04-15 13:21:53 -0700

    Fix for teamId values being null for projects returned as a list.

--- server/src/main/java/com/cooksys/server/mappers/TeamMapper.java
+++ server/src/main/java/com/cooksys/server/mappers/TeamMapper.java
@@ -3,16 +3,18 @@ package com.cooksys.server.mappers;
 import com.cooksys.server.entities.Team;
 import com.cooksys.server.models.TeamDto;
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
 
 import java.util.List;
 
-@Mapper(componentModel = "spring")
+@Mapper(componentModel = "spring", uses = { ProjectMapper.class, UserMapper.class })
 public interface TeamMapper {
 
     TeamDto entityToDto(Team team);
 
     List<TeamDto> entitiesToDtos(List<Team> teams);
 
+    @Mapping(target = "deleted", ignore = true)
 	Team DtoToEntity(TeamDto team);
 
 }
