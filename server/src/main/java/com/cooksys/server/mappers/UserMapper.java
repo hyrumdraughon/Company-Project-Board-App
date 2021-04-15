ecfeb431a37b40ba797112b9a119c97506daaf03
Julio Corzo <juliocorzo@me.com>
2021-04-15 13:22:15 -0700

    Preemptive fix for id values being null for users returned as a list.

--- server/src/main/java/com/cooksys/server/mappers/UserMapper.java
+++ server/src/main/java/com/cooksys/server/mappers/UserMapper.java
@@ -9,10 +9,22 @@ import com.cooksys.server.entities.User;
 import com.cooksys.server.models.CreateUserDto;
 import com.cooksys.server.models.UserDto;
 
-@Mapper(componentModel = "spring", uses = {ProfileMapper.class, CredentialMapper.class})
+@Mapper(componentModel = "spring", uses = { ProfileMapper.class, CredentialMapper.class, CompanyMapper.class, TeamMapper.class })
 public interface UserMapper {
 
+    @Mapping(target = "userTeam", ignore = true)
+    @Mapping(target = "userCompany", ignore = true)
+    @Mapping(target = "updated", ignore = true)
+    @Mapping(target = "role", ignore = true)
+    @Mapping(target = "id", ignore = true)
+    @Mapping(target = "created", ignore = true)
+    @Mapping(target = "active", ignore = true)
     User dtoToEntity(CreateUserDto createUserDto);
+
+    @Mapping(target = "userTeam", ignore = true)
+    @Mapping(target = "userCompany", ignore = true)
+    @Mapping(target = "credentials", ignore = true)
+    @Mapping(target = "active", ignore = true)
     User dtoTo_get_Entity(UserDto userDto);
 
     @Mapping(target= "email", source="credentials.email")
