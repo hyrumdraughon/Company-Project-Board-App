d973c5f207be0520d3f96b797383e91a73805914
Austin Howard <cnfishead@hotmail.com>
2021-04-21 13:38:58 -0400

    implemented GET team users

--- client/src/context/TeamProvider.js
+++ client/src/context/TeamProvider.js
@@ -1,81 +1,83 @@
-import React, {useState, useContext, useEffect} from 'react'
-import { UserContext } from '../context/UserProvider'
+import React, { useState, useContext, useEffect } from "react";
+import { UserContext } from "../context/UserProvider";
 
-import axios from 'axios'
+import axios from "axios";
 
-export const TeamContext = React.createContext()
+export const TeamContext = React.createContext();
 
 function TeamProvider(props) {
+  const initState = {
+    team: { name: "", text: "" },
+    teamId: localStorage.getItem("TeamId") || 0,
+    projects: [],
+    users: [],
+  };
 
-    const initState = {
-        team: {name: '', text: ''},
-        teamId: localStorage.getItem("TeamId") ||  0,
-        projects: [],
-        users: []
-    }
+  const { user } = useContext(UserContext);
+  const teamId = user.teamId;
 
-    const {user} = useContext(UserContext)
-    const teamId = user.teamId
+  const [teamState, setTeamState] = useState(initState);
 
-    const [teamState, setTeamState] = useState(initState)
+  axios.defaults.baseURL = "https://api.juliocorzo.com";
 
-    axios.defaults.baseURL = 'https://api.juliocorzo.com'
+  // console.log(teamState)
 
-    // console.log(teamState)
+  const createTeam = (data) => {
+    const newTeam = {
+      name: data.name,
+      text: data.text,
+      companyId: data.companyId,
+    };
+    axios.post("/team", newTeam);
+  };
 
-    const createTeam = (data) => {
-        const newTeam = {
-            name: data.name,
-            text: data.text,
-            companyId: data.companyId
-        }
-        axios.post("/team", newTeam)
-    }
+  const getTeam = () => {
+    axios.get(`/team/${teamId}`).then((res) => {
+      const { teamName, text, projects, id, members } = res.data;
+      setTeamState((prevState) => ({
+        ...prevState,
+        team: {
+          name: teamName,
+          text: text,
+        },
+        teamId: id,
+        projects: projects,
+        users: members,
+      }));
+    });
+  };
+  const getTeamUsers = () => {
+      return JSON.parse(axios.get(`/team/${teamState.teamid}/users`))
+  }
 
-    
+  const getProject = () => {
+    axios
+      .get(`/team/${teamId}/projects`)
+      .then((res) => res.data)
+      .then((listOfProjects) =>
+        setTeamState((prevState) => ({
+          ...prevState,
+          projects: listOfProjects,
+        }))
+      );
+  };
 
-    const getTeam = () => {
-        axios.get(`/team/${teamId}`)
-            .then( res => {
-                const {teamName, text, projects, id, members} = res.data
-                // localStorage.setItem("TeamId", id)
-                setTeamState(prevState => ({
-                    ...prevState,
-                    team: {
-                        name: teamName,
-                        text: text
-                    },
-                    teamId: id,
-                    projects: projects,
-                    users: members
-                }))
-            })
-    }
-
-    const getProject = () => {
-        axios.get(`/team/${teamId}/projects`)
-            .then( res => res.data)
-            .then(listOfProjects => setTeamState(
-                    prevState => ({
-                        ...prevState, projects: listOfProjects
-                    })
-                )
-            )
-    }
-
-    return (
-        <TeamContext.Provider value={{
-            team: teamState.team,
-            teamId: teamState.teamId,
-            projects: teamState.projects,
-            users: teamState.users,
-            getTeams: getTeam,
-            getProjects: getProject,
-            createTeam: createTeam
-        }} >
-            {props.children}
-        </TeamContext.Provider>
-    )
+  return (
+    <TeamContext.Provider
+      value={{
+        team: teamState.team,
+        teamId: teamState.teamId,
+        projects: teamState.projects,
+        users: teamState.users,
+        getTeams: getTeam,
+        getProjects: getProject,
+        createTeam: createTeam,
+        getTeamUsers: getTeamUsers
+      }}
+    >
+      {props.children}
+    </TeamContext.Provider>
+  );
 }
 
-export default TeamProvider
+export default TeamProvider;
