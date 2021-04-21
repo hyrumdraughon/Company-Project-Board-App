335ce0685759a9a3674cc0e0b5cafaa9d1c9b202
ThomasDeSantis <40611847+ThomasDeSantis@users.noreply.github.com>
2021-04-21 12:49:31 -0400

    Add form for adding team.

---
+++ client/src/components/PostTeamForm.jsx
@@ -0,0 +1,87 @@
+import { Grid } from "@material-ui/core"
+import './profile.css'
+import Button from './Button'
+
+import { useContext} from 'react'
+import { UserContext } from '../context/UserProvider'
+import {useState} from 'react'
+import { Redirect } from "react-router"
+
+//TODO: Handle button handler when endpoint is defined
+//PROPS
+/*
+{
+    userId: integer that contains the id of the user
+}
+*/
+const PostTeamForm = (props) => {
+    const {user} = useContext(UserContext)
+
+    const [name,updateName] = useState()
+
+    const nameSubmission = (event) => {
+        updateName(event.target.value)
+        console.log(name)
+    }
+
+    const [text,updateText] = useState()
+
+    const textSubmission = (event) => {
+        updateText(event.target.value)
+        console.log(text)
+    }
+
+    const sendFormRequest = (event) => {
+        event.preventDefault()
+
+        let nameSubmit = name
+        if(name !==  undefined || user.companyId !== undefined){
+            const request = {
+                name: name,
+                text: text,
+                companyId: user.companyId
+            }
+            console.log(request)
+            //Put POST function here sending request
+        }
+
+        
+    }
+
+    return(
+        <section className = "projectContainer">
+            <div className = "topMargin"/>
+            <form className = "formContainer " onSubmit={sendFormRequest}>
+                <Grid
+                    container
+                    direction="column"
+                    justify="center"
+                    alignItems="center"
+                >
+                    <div className="flexContainer">
+                        <div className="fieldContainer">
+                            <p>Name:</p>
+                        </div>
+                        <div className="fieldContentContainer">
+                            <input placeholder = 'Input team name here' type='text' onChange={nameSubmission}/> 
+                        </div>
+                        <div className = "emptyCenterer"/>
+                    </div>
+                    <div class="flexContainer">
+                        <div className="fieldContainer">
+                            <p>Text:</p>
+                        </div>
+                        <div className="fieldContentContainer">
+                            <input placeholder = 'Input team text here' type='text' onChange={textSubmission}/> 
+                        </div>
+                        <div className = "emptyCenterer"/>
+                    </div>
+                    <div className = "buttonMargin"/>
+                    <Button type="submit" label = "Submit"/>
+                </Grid>
+            </form>
+        </section>
+    )
+}
+
+export default PostTeamForm
---
+++ client/src/components/PostTeamFormPage.js
@@ -0,0 +1,15 @@
+import NavBar from './NavBar'
+import PostTeamForm from './PostTeamForm'
+
+const PostTeamFormPage = () => {
+    return(
+        <body>
+            <div className = 'page-container'>
+                <NavBar></NavBar>
+                <PostTeamForm/>
+            </div>
+        </body>
+    )
+}
+
+export default PostTeamFormPage
